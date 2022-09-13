c519e23cf0f1acf37946ee9a4582dbe0c1466e9b
Asep Riki <asepriki2014@gmail.com>
2022-09-13 17:11:06 +0700

    contoh library sederhana composer

---
+++ src/Custumer.php
@@ -0,0 +1,14 @@
+<?php
+namespace Riki\Belajar;
+
+class Custumer{
+
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name):string{
+        return "Hello $name my name is $this->name";
+    }
+}
